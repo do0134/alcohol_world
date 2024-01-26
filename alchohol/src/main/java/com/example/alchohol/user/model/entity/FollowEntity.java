a832bf82c10932497e60840ab56ce15d119526b5
do0134 <do0134@naver.com>
2024-01-27 00:55:24 +0900

    feat: id 전략 변경

--- alchohol/src/main/java/com/example/alchohol/user/model/entity/FollowEntity.java
+++ alchohol/src/main/java/com/example/alchohol/user/model/entity/FollowEntity.java
@@ -10,7 +10,7 @@ import lombok.Getter;
 public class FollowEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne
