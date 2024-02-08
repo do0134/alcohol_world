699f3cef4489f6f40af365fa7b663bbdcb06c3fe
do0134 <do0134@naver.com>
2024-02-08 21:30:22 +0900

    feat: sales item 컨트롤러 개발

--- alcohol/item_service/src/main/java/com/example/item_service/controller/ItemController.java
+++ alcohol/item_service/src/main/java/com/example/item_service/controller/ItemController.java
@@ -2,6 +2,8 @@ package com.example.item_service.controller;
 
 import com.example.common.response.Response;
 import com.example.item_service.model.dto.Item;
+import com.example.item_service.model.dto.SalesItem;
+import com.example.item_service.model.dto.SalesItemRequest;
 import com.example.item_service.service.ItemService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -21,5 +23,17 @@ public class ItemController {
     public Response<Item> getItem(@PathVariable("itemId") Long itemId) {
         Item item = itemService.getItem(itemId);
         return Response.success(item);
+    }
+
+    @PostMapping("/{itemId}")
+    public Response<Void> createSalesItem(@PathVariable("itemId") Long itemId, @RequestBody SalesItemRequest salesItemRequest) {
+        itemService.createSalesItem(itemId,salesItemRequest.getItemType(), salesItemRequest.getPrice(), salesItemRequest.getStock(),salesItemRequest.getStartTime(),salesItemRequest.getEndTime());
+        return Response.success();
+    }
+
+    @GetMapping("/{salesItemId}")
+    public Response<SalesItem> getSalesItem(@PathVariable("salesItemId") Long salesItemId) {
+        SalesItem salesItem = itemService.getSalesItem(salesItemId);
+        return Response.success(salesItem);
     }
 }
