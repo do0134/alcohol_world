d223bf0d4caa9491ef9c618b0ec9161a301fc4db
do0134 <do0134@naver.com>
2024-02-12 22:38:38 +0900

    feat: common 모듈 bean 주입

--- alcohol/item_service/src/main/java/com/example/item_service/ItemServiceApplication.java
+++ alcohol/item_service/src/main/java/com/example/item_service/ItemServiceApplication.java
@@ -3,7 +3,9 @@ package com.example.item_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(
+        scanBasePackages = {"com.example.common", "com.example.item_service"}
+)
 public class ItemServiceApplication {
 
     public static void main(String[] args) {
--- alcohol/order_service/src/main/java/com/example/order_service/OrderServiceApplication.java
+++ alcohol/order_service/src/main/java/com/example/order_service/OrderServiceApplication.java
@@ -4,7 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+@SpringBootApplication(
+        scanBasePackages = {"com.example.common", "com.example.order_service"}
+)
 @EnableFeignClients
 public class OrderServiceApplication {
 
