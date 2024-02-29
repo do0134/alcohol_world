8befe132f0a9782bd01abbb8f2cade8020b8ff0b
do0134 <do0134@naver.com>
2024-02-29 15:27:45 +0900

    feat: heartbeat pub/sub 구조 수정

--- testCode/order_pay_test_final.py
+++ testCode/order_pay_test_final.py
@@ -3,6 +3,7 @@ import random
 from concurrent.futures import ThreadPoolExecutor
 import time
 import redis
+from collections import defaultdict
 
 
 BASE_URL = "http://localhost:8085/api/v1/order"
@@ -14,70 +15,67 @@ pay_exception = 0
 error = 0
 heartbeat_error = 0
 dont_pay = 0
+pay_low_quantity = 0
+order_low_quantity = 0
+error_type = defaultdict(int)
 
 
-def send_heartbeat(redis_conn, userId, itemId):
+def send_heartbeat(userId, itemId):
+    redis_conn = redis.Redis("localhost", 6379)
     channel_name = f"User {userId} order Item {itemId}"
+    alive_response = channel_name + " is alive"
     p = redis_conn.pubsub()
-    p.subscribe(channel_name)
+    p.subscribe(["heartbeat"])
+
     for message in p.listen():
         if message['type'] == 'message':
             m = message['data'].decode('utf-8')
             if m == channel_name:
-                redis_conn.publish(channel_name, "alive")
+                redis_conn.publish("heartbeat", alive_response)
+                break
 
 
 def send_order_requests(userId):
-    global order_exception, pay_exception, error, heartbeat_error, dont_pay
+    global order_exception, pay_exception, error, heartbeat_error, dont_pay, pay_low_quantity, order_low_quantity
     try:
         # 아이템 Id 뽑기
         itemId = random.randint(min_v, max_v)
-        
+
         # Order 요청 보내기
         create_order_response = requests.post(f"{BASE_URL}/{userId}/{itemId}")
         create_order_result_code = create_order_response.json().get("resultCode")
+
         # Order 요청 실행 안됐으면 반환
         if create_order_result_code != "SUCCESS":
+            error_type[create_order_result_code] += 1
             order_exception += 1
-            print(f"Failed to create order for userId {userId} and itemId {itemId} because {create_order_result_code}")
             return
 
         # 20% 확률로 사용자는 결제하지 않음(요구사항)
         percentage = random.randint(0,99)
 
+
         if percentage < 20:
             dont_pay += 1
 
         else:
-            r = redis.Redis("localhost", 6379)
             # 결제 창에 있다는 것을 알리는 HeartBeat 작동
-            send_heartbeat(r,userId, itemId)
+            # send_heartbeat(userId, itemId)
             # 결제 요청 보내기
             pay_response = requests.post(f"{BASE_URL}/pay/{userId}/{itemId}")
-            r.pubsub().unsubscribe(f"User {userId} order Item {itemId}")
-            r.close()
             pay_result_code = pay_response.json().get("resultCode")
             if pay_result_code != "SUCCESS":
-                if pay_result_code == "NO_SUCH_ORDER":
-                    heartbeat_error += 1
-                else:
-                    # print(f"Failed to pay for order for userId {userId} and itemId {itemId}")
-                    pay_exception += 1
+                error_type[create_order_result_code] += 1
             else:
                 my_dict[itemId] += 1
     except Exception as e:
-        print(f"Error sending requests for userId {userId}: {e}")
-        itemId = random.randint(min_v, max_v)
-        error += 1
-        r = redis.Redis("localhost", 6379)
-        r.pubsub().unsubscribe(f"User {userId} order Item {itemId}")
-        r.close()
+        error_type[str(e)] += 1
 
 
 def main():
     global my_dict, order_exception, pay_exception, error
     # request보낼 수 == user의 수
-    num_requests = 1000
+    num_requests = 10000
 
     start_time = time.time()
 
@@ -93,10 +91,11 @@ def main():
     elapsed_time = end_time - start_time
     print(f"Elapsed time: {elapsed_time} seconds")
     print(f"item used: {my_dict}")
-    print(f"Failed because Exception: order {order_exception}, pay {pay_exception}")
-    print(f"error: {error}")
+
+    for key in error_type.keys():
+        print(f"{key}: {error_type[key]}")
+
     print(f"Customer don't pay item {dont_pay}")
-    print(heartbeat_error)
 
 
 if __name__ == "__main__":
